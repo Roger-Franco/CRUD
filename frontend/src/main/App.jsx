5ca0937e3518fb237b537643324069f874765112
Roger-Franco <rogerfrancogalt@gmail.com>
2022-07-04 10:18:18 -0400

    Implementando rotas

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -3,15 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css'
 import React from 'react'
+// import { HashRouter } from 'react-router-dom' // com o HashRouter, usa-se a estratégia do hash. Ele substitui o BrowserRouter;
+import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './Routes'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Footer from '../components/template/Footer'
-import Home from '../components/home/Home';
 
 export default props =>
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Home />
-    <Footer />
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </BrowserRouter>
